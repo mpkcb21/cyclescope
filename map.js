74a8e219a2bb1b40a176436692cb9a439ba902b7
mpkcb21 <mpudukottaikrishnara@ucsd.edu>
2026-05-16 14:30:54 -0700

    Update map.js

--- map.js
+++ map.js
@@ -16,7 +16,7 @@ let departuresByMinute = Array.from({ length: 1440 }, () => []);
 let arrivalsByMinute   = Array.from({ length: 1440 }, () => []);
 
 function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat);
   const { x, y } = map.project(point);
   return { cx: x, cy: y };
 }
